d5aa357e02b03c020bb2f21d4c5e9dfd2a5f9f21
Wale <onafesowale@gmail.com>
2023-04-08 03:54:12 -0700

    Developer info

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import Button from '../components/Button';
 import style from '../styles/Home.module.css';
 import { useEffect } from 'react';
-import jwtDecode from 'jwt-decode';
 import { checkTokenForExpiry } from '../helper/coreFunction';
 
 
@@ -16,18 +15,33 @@ function Home() {
 
   }, [])
 
+  var curWidth =window.innerWidth
+
+  // console.log(curWidth);
+
+  var desiredWidth = 400;
+
+  var phoneButton = curWidth < desiredWidth;
+
   return (
     <div className={style.container}>
       <div className={style.mainWrapper}>
-        <h1 className = {`${style.mainTitle} ${style.mainTitleEffect}`}>gtcc bible quiz</h1>
-        
+        <h1 className={`${style.mainTitle} ${style.mainTitleEffect}`}>
+          gtcc bible quiz
+        </h1>
 
         <Link to="/category">
-          <Button name="Get started">
+          <Button
+            name="Get started"
+            fontSize={phoneButton && "1.5rem"}
+            borderRadius={phoneButton && "20px"}
+            padding={phoneButton && ".7rem 1.5rem"}
+          >
             <i class="fa fa-play-circle" aria-hidden="true"></i>
           </Button>
         </Link>
       </div>
+      <p className={style.developer}>Developed by <a href="mailto:onafesowale@gmail.com">Fesor</a></p>
     </div>
   );
 }
