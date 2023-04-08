c7c7d91ac05205491d948209a6a4b1f848d2b7fd
Wale <onafesowale@gmail.com>
2023-04-08 03:53:04 -0700

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import Home from "./pages/Home";
 import Category from "./pages/Category";
 import ThousandQuestions from "./pages/ThousandQuestions";
 import {
-  createBrowserRouter,
-  RouterProvider,
-  Router,
   Routes,
   Route,
   BrowserRouter,
@@ -13,7 +10,6 @@ import RevisionQuestions from "./pages/RevisionQuestions";
 import FesorQuestions from "./pages/FesorQuestions";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
-import RequireAuth from "./components/Auth/requireAuth";
 import Admin from "./pages/Admin";
 
 function App() {
