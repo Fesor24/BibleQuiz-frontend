2ce5f29213a76ed346c010077d503ac914079217
Wale <onafesowale@gmail.com>
2023-04-21 10:35:29 -0700

    Added routes

--- src/api/ApiRoutes.js
+++ src/api/ApiRoutes.js
@@ -1,7 +1,7 @@
 export default class ApiRoutes {
   static BASE_URL = "https://localhost:7230/";
 
-  static BASE_URL_LIVE = "api"
+  static BASE_URL_LIVE = "api";
 
   static FetchThousandQuestions = "api/thousand-questions/fetch";
 
@@ -20,4 +20,6 @@ export default class ApiRoutes {
   static FetchUser = "api/fetch-user";
 
   static GrantAccess = "api/grant-access";
+
+  static DeleteQuestions = "api/revision-question/delete";
 }
