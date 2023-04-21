41cf2e1b679a4b60fe6983e6fbdfd3118eec0c07
Wale <onafesowale@gmail.com>
2023-04-21 10:36:34 -0700

    timer component

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -6,36 +6,38 @@ function Timer({
   handleNextButtonClick,
   handleSaveButtonClick,
   handleResetButtonClick,
+  isRevisionQuestionSection,
+  handleDeleteRevisionQuestions
 }) {
   return (
-    
-      <div className={style.timer}>
-        {/* <Circle borderWidth ={borderWidth} circleText= {"00:30"} colorText={colorText} /> */}
+    <div className={style.timer}>
+      {/* <Circle borderWidth ={borderWidth} circleText= {"00:30"} colorText={colorText} /> */}
 
-        <p className={style.timerText}>
-          00:
-          {countdown === 0
-            ? "00"
-            : countdown < 10
-            ? `0${countdown}`
-            : countdown}
-        </p>
+      <p className={style.timerText}>
+        00:
+        {countdown === 0 ? "00" : countdown < 10 ? `0${countdown}` : countdown}
+      </p>
 
-        <div class={style.btnGroup}>
-          <Button click={handleNextButtonClick}>
-            <i class="fa fa-arrow-right" aria-hidden="true"></i>
+      <div class={style.btnGroup}>
+        <Button click={handleNextButtonClick}>
+          <i class="fa fa-arrow-right" aria-hidden="true"></i>
+        </Button>
+        &nbsp; &nbsp;
+        <Button click={handleSaveButtonClick}>
+          <i class="fa fa-arrow-left"></i>
+        </Button>
+        &nbsp; &nbsp;
+        <Button click={handleResetButtonClick}>
+          <i class="fa-solid fa-ban"></i>
+        </Button>
+        &nbsp; &nbsp;
+        {isRevisionQuestionSection && (
+          <Button click={handleDeleteRevisionQuestions}>
+            <i class="fa-solid fa-trash"></i>
           </Button>
-          &nbsp; &nbsp;
-          <Button click={handleSaveButtonClick}>
-            <i class="fa fa-arrow-left"></i>
-          </Button>
-          &nbsp; &nbsp;
-          <Button click={handleResetButtonClick}>
-            <i class="fa-solid fa-ban"></i>
-          </Button>
-        </div>
+        )}
       </div>
-    
+    </div>
   );
 }
 
